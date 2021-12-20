46d872fd41f81a69437c121da54c5d05033c9ef8
ZyphonSecretSanta2021 <96397860+ZyphonSecretSanta2021@users.noreply.github.com>
2021-12-19 19:27:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = async (stimulus, context = [], language) => {
 
     if (cookies == null) {
         // we must get the XVIS cookie before we can make requests to the API
-        const req = await superagent.get("https://www.cleverbot.com/").set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36");
+        const req = await superagent.get("https://www.cleverbot.com/");
         cookies = req.header["set-cookie"]; // eslint-disable-line require-atomic-updates
     }
 
@@ -41,7 +41,6 @@ module.exports = async (stimulus, context = [], language) => {
         try {
             const req = await superagent.post(`https://www.cleverbot.com/webservicemin?uc=UseOfficialCleverbotAPI${cbsid ? `&out=${encodeURIComponent(lastResponse)}&in=${encodeURIComponent(stimulus)}&bot=c&cbsid=${cbsid}&xai=${xai}&ns=2&al=&dl=en&flag=&user=&mode=1&alt=0&reac=&emo=&sou=website&xed=&` : ""}`)
                 .set("Cookie", `${cookies[0].split(";")[0]}; _cbsid=-1`)
-                .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
                 .type("text/plain")
                 .send(payload);
 
